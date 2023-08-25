e08469961f2460b4f5eaf39b5fdfdc66b7403bff
Sly03 <slydevwebproject@outlook.fr>
2023-08-25 16:50:00 +0200

    Ajoutez des fichiers projet.

---
+++ Store.Core/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Core
+{
+    public class Product
+    {
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
